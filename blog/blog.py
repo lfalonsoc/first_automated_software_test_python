0bd169d75a0f8a0966aa0179d17a7809805a6b76
Luis Fernando Alonso Castiblanco <lfalonsoc@gmail.com>
2024-02-13 10:42:50 -0500

    test_the__repr__method_multiple_post

--- blog/blog.py
+++ blog/blog.py
@@ -6,3 +6,15 @@ class Blog:
         self.title: str = title
         self.author: str = author
         self.post: List[Any] = []
+    
+    def __repr__(self) -> str:
+        return '{} by {} ({} post{})'.format(self.title, 
+                                            self.author,
+                                            len(self.post),
+                                            's' if len(self.post) != 1 else '')
+    
+    def create_post(self, title: str, content: str):
+        pass
+    
+    def json(self):
+        pass
--- blog/tests/unit/blog_test.py
+++ blog/tests/unit/blog_test.py
@@ -9,3 +9,19 @@ class BlogTest(TestCase):
         self.assertEqual('Test Author', b.author)
         self.assertListEqual([], b.post)
         self.assertEqual(0, len(b.post))
+        
+    def test_repr(self):
+        b: Blog = Blog('Test', 'Test Author')
+        b2: Blog = Blog('My day', "Rolf")
+        
+        self.assertEqual(b.__repr__(), 'Test by Test Author (0 posts)')
+        self.assertEqual(b2.__repr__(), 'My day by Rolf (0 posts)')
+    
+    def test_repr_multiple_posts(self):
+        b: Blog = Blog('Test', 'Test Author')
+        b.post = ['test']
+        b2: Blog = Blog('My day', "Rolf")
+        b2.post = ['Test', "Another"]
+        
+        self.assertEqual(b.__repr__(), 'Test by Test Author (1 post)')
+        self.assertEqual(b2.__repr__(), 'My day by Rolf (2 posts)')
