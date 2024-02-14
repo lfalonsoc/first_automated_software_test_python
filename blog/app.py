b197b4c29c4c769036031678d4a4808f0a547ce0
Luis Fernando Alonso Castiblanco <lfalonsoc@gmail.com>
2024-02-13 21:30:44 -0500

    Taking patching further

--- blog/app.py
+++ blog/app.py
@@ -1,7 +1,15 @@
 from typing import Any
 
+from blog.blog import Blog
+
 MENU_PROMPT = 'Enter "c" to create a blog, "l" to list blogs, \
 "r" to read one, "p" to create post, or "q" to quit.'
+POST_TEMPLATE = """
+--- {} ---
+
+{}
+
+"""
 
 blogs: dict[Any, Any] = dict()  # blog_name : Blog object
 
@@ -15,8 +23,39 @@ def menu() -> None:
     """
     print_blogs()  # 1.
     selection: str = input(MENU_PROMPT)  # 2.
+    while selection != "q":
+        if selection == "c":
+            ask_create_blog()
+        elif selection == "l":
+            print_blogs()
+        elif selection == "r":
+            ask_read_blog()
+        elif selection == "p":
+            ask_create_post()
+        selection: str = input(MENU_PROMPT)
 
 
 def print_blogs() -> None:
     for key, blog in blogs.items():
         print("- {}".format(blog))
+
+
+def ask_create_blog() -> None:
+    title: str = input("Enter your blog title: ")
+    author: str = input("Enter your name: ")
+
+    blogs[title] = Blog(title, author)
+
+
+def ask_read_blog() -> None:
+    title: str = input("Enter the blog title you want to red: ")
+
+    print_posts(blogs[title])
+
+
+def print_posts(post: Any) -> None:
+    print(POST_TEMPLATE.format(post.title, post.content))
+
+
+def ask_create_post() -> None:
+    pass
--- blog/tests/system/test_app.py
+++ blog/tests/system/test_app.py
@@ -4,6 +4,7 @@ from unittest.mock import patch
 
 import app
 from blog.blog import Blog
+from blog.post import Post
 
 
 class TestsApp(TestCase):
@@ -24,3 +25,41 @@ class TestsApp(TestCase):
         with patch("builtins.print") as mocked_print:
             app.print_blogs()
             mocked_print.assert_called_with("- Test by Test Author (0 posts)")
+
+    def test_ask_create_blog(self) -> None:
+        with patch("builtins.input") as mocked_input:
+            mocked_input.side_effect = ("Test", "Test Author")
+            app.ask_create_blog()
+
+            self.assertIsNotNone(app.blogs.get("Test"))
+
+    def test_ask_read_blog(self) -> None:
+        blog: Blog = Blog("Test", "Test Author")
+        app.blogs = {"Test": blog}
+        with patch("builtins.input", return_value="Test"):
+            with patch("app.print_posts") as mocked_print_posts:
+                app.ask_read_blog()
+
+                mocked_print_posts.assert_called_with(blog)
+
+    def test_print_posts(self) -> None:
+        blog: Blog = Blog("Test", "Test Author")
+        app.blogs = {"Test": blog}
+
+        with patch("builtins.print") as mocked_print_post:
+            app.print_posts(blog)
+
+            mocked_print_post.assert_called_with(blog.posts[0])
+
+    def test_print_post(self) -> None:
+        post: Post = Post("Pist title", "Post content")
+        expect_print: str = """
+--- Post title ---
+
+Post content
+
+"""
+        with patch("builtins.print") as mocket_print:
+            app.print_posts(post)
+
+            mocket_print.assert_called_with(expect_print)
